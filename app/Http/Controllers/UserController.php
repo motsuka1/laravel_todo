ee7e9ebad003d8feced0bf9d93e2dd1a709d5553
Masaki Otsuka <motsuka1996@gmail.com>
2020-12-10 10:19:25 +0900

    crud with sql query and ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     //
     public function index()
-    {
+    {   
+        // CRUD with SQL query
+
+        // DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['Taro', 'taro@example.com', 'password']);
+        // DB::update('update users set name = ? where id = 1', ['Taro Yamada']);
+        // DB::delete('delete from users where id = 1');
+        // $users = DB::select('select * from users');
+        // return $users;
+
+
+        // CRUD with ORM
+
+        // $user = new User;
+        // $user->name = 'Hanako Yamada';
+        // $user->email = 'hanako@example.com';
+        // $user->password = 'password';
+        // $user->save();
+
+        // User::create([
+        //     'name' => 'Hanako Yamada',
+        //     'email' => 'hanako@example.com',
+        //     'password' => 'password',
+        // ]);
+
+        // $user = User::all();
+        // return $user;
+
+        // User::where('name', 'Hanako Yamada')->update(['name' => 'Hanako Sato']);
+
+        // User::where('name', 'Hanako Sato')->delete();
+
         return view('home');
     }
 }
