056b4b02bef7362074f257b693c791eaa7d81e2b
Masaki Otsuka <motsuka1996@gmail.com>
2020-12-11 13:53:39 +0900

    add laravel ui bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/user', [UserController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
